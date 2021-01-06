a27ba1b26693394e5a6d85dec2b0c2595a2e3a64
Alma-Co <edith.cantegrit@gmail.com>
2021-01-06 10:14:44 +0100

    name app changed

--- astronauts-agenda/src/redux/actions/actions-types.js
+++
@@ -1,7 +0,0 @@
-const actionTypes = {
-  LOAD_ASTRONAUTS_LIST: 'LOAD_ASTRONAUTS_LIST',
-  LOAD_LIST_ERROR: 'LOAD_LIST_ERROR'
-};
-  
-  export default actionTypes;
-  
--- astronauts-agenda/src/redux/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import astronautsReducer from './astronautsReducer';
-
-const rootReducer = combineReducers({ astronautsReducer });
-
-export default rootReducer;
